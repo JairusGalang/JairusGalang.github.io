bce5c3f11253714ca41159522fdd1a42e6dcbbdc
Jairus Galang <jairusgalang.dev@gmail.com>
2022-04-21 23:06:52 +0800

    Fixed Overflow Issues(Web/Mobile View) / Updated Animations

--- js/style.js
+++ js/style.js
@@ -1,11 +1,5 @@
 const toggleMenubtn = document.querySelector('.toggleMenubtn');
 const menuBar = document.querySelector('.menuBar');
-// const firstDiv = document.querySelector('.firstDiv');
-// const secondDiv = document.querySelector('.secondDiv');
-// const thirdDiv = document.querySelector('.thirdDiv')
-// const fourthDiv = document.querySelector('.fourthDiv')
-
-
 toggleMenubtn.addEventListener('click', () =>{
   menuBar.classList.toggle("hidden");
 })
